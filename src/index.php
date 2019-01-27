3d792cb3d17719927c386b78fa6e3dcfb88af742
Bryce <bryce.tavares@gmail.com>
2019-01-27 18:17:06 +0100

    ajout deconnexion

--- src/index.php
+++ src/index.php
@@ -1,10 +1,11 @@
 <?php session_start() ?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
@@ -20,8 +21,17 @@
         <input type="submit" name="submit" value="submit">
     </form>
     <a href="inscription.php">Sign In</a>
-<?php else: echo 'Bonjour '.$_SESSION['user']; endif; ?>
-    <table border="1">
+
+<?php else: echo 'Bonjour '.$_SESSION['user']; ?>
+
+    <form action="traitConnexion.php" method="post">
+        <input type="submit" name="subDeco" value="Déconnexion">
+    </form>
+    <a href="addArticle.php"><input type="button" name="addArticle" value="Ajouter un article"></a>
+
+<?php endif; ?>
+    
+    <table class="table" border="1">
        <th>Title</th>
        <th>Description</th> 
     </table>
--- src/traitConnexion.php
+++ src/traitConnexion.php
@@ -1,8 +1,13 @@
 <?php
+session_start();
 require_once 'fonction.php';
 
 if(!empty($_POST['username']) && !empty($_POST['password'])){
     $username = $_POST['username'];
     $password = $_POST['password'];
     verifConnexion($username, $password);
+}
+if($_POST['subDeco']){
+    session_destroy();
+    header('Location: index.php');
 }
